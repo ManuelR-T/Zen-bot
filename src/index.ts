cc217f90362da1cf0357b42e5f4cd2ebb06da105
Manuel Tomé <105550975+ManuelR-T@users.noreply.github.com>
2023-09-03 02:31:00 +0200

    feat: handle timezones (#8)

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,26 @@ import { handleCommands } from './commands/index'
 import { initializeBot, client } from './init'
 import zenCountSchema from './schemas/zenCountSchema'
 
+const isDSTinParis = (date: Date) => {
+  // Daylight Saving Time starts on the last Sunday of March
+  const dstStart = new Date(date.getFullYear(), 2, 31, 2 - 1)
+  dstStart.setDate(31 - dstStart.getDay())
+
+  // Daylight Saving Time ends on the last Sunday of October
+  const dstEnd = new Date(date.getFullYear(), 9, 31, 3 - 1)
+  dstEnd.setDate(31 - dstEnd.getDay())
+
+  return date >= dstStart && date < dstEnd
+}
+
 const isMirrorTime = (): boolean => {
-  const now = new Date()
+  let now = new Date()
   const gmt = now.getTimezoneOffset() / 60
-  return now.getHours() === now.getMinutes() + 2 - gmt
+  const offset = isDSTinParis(now) ? 2 : 1
+
+  now = new Date(now.getTime() + (offset + gmt) * 3600000)
+
+  return now.getHours() === now.getMinutes()
 }
 
 const handleNezMessage = async (message: Message): Promise<void> => {
