041b363bb5d231e3ed4ed56e368679d90380bdf6
Manuel Tomé <manuel.tome@epitech.eu>
2023-11-19 03:01:39 +0100

    feat: basic RPS game

---
+++ src/commands/games/index.ts
@@ -0,0 +1,27 @@
+import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+
+import { Command } from "@/types";
+
+import subCommandHandler from "../utils/subcommandHandler";
+import subcommandMap from "./subcommandMap";
+
+const data = new SlashCommandBuilder()
+  .setName("games")
+  .setDescription("Play games")
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName("rps")
+      .setDescription("Play rock paper scissors")
+      .addUserOption((option) =>
+        option
+          .setName("user")
+          .setDescription("The user to play against (optional)")
+          .setRequired(false)
+      )
+  )
+
+const execute = async (interaction: CommandInteraction): Promise<void> => {
+  await subCommandHandler(interaction, subcommandMap);
+};
+
+export default { data, execute, cooldown: 5 } as Command;
