62427dd2265cb1faaf21aaf44cbb6d16906fa87d
Manuel Tomé <manuel.tome@epitech.eu>
2023-11-19 03:28:47 +0100

    refactor

--- src/commands/games/rps/addCollector.ts
+++ src/commands/games/rps/addCollector.ts
@@ -13,8 +13,8 @@ function isChoice(choice: string): choice is Choice {
 }
 
 const rpsResolver = (
-  player1Choice: string,
-  player2Choice: string,
+  player1Choice: Choice,
+  player2Choice: Choice,
 ): 'tie' | 'player1' | 'player2' => {
   if (player1Choice === player2Choice) return 'tie'
 
@@ -24,10 +24,7 @@ const rpsResolver = (
     scissors: 'paper',
   }
 
-  return winsAgainst[player1Choice as keyof typeof winsAgainst] ===
-    player2Choice
-    ? 'player1'
-    : 'player2'
+  return winsAgainst[player1Choice] === player2Choice ? 'player1' : 'player2'
 }
 
 export default async (
@@ -37,10 +34,7 @@ export default async (
   player1: User,
   player2: User | null,
 ): Promise<void> => {
-  const collector = message.createMessageComponentCollector({
-    time: 60000,
-  })
-
+  const collector = message.createMessageComponentCollector({ time: 60000 })
   let player1Choice: Choice | null = null
   let player2Choice: Choice | null = null
 
@@ -49,12 +43,11 @@ export default async (
 
     if (i.user.id === player1.id) {
       player1Choice = i.customId
-      if (player2 === null) {
-        player2Choice = (['rock', 'paper', 'scissors'] as Choice[])[
-          Math.floor(Math.random() * 3)
-        ]
+      if (!player2) {
+        const choices: Choice[] = ['rock', 'paper', 'scissors']
+        player2Choice = choices[Math.floor(Math.random() * choices.length)]
       }
-    } else if (i.user.id === player2?.id) {
+    } else if (player2 && i.user.id === player2.id) {
       player2Choice = i.customId
     } else {
       return
@@ -71,7 +64,7 @@ export default async (
 
     player2 = player2 ?? message.client.user
 
-    const emojiMap: { [key in Choice]: string } = {
+    const emojiMap = {
       rock: '🪨',
       paper: '📜',
       scissors: '✂️',
@@ -80,19 +73,17 @@ export default async (
     const player1Emoji = player1Choice ? emojiMap[player1Choice] : '❓'
     const player2Emoji = player2Choice ? emojiMap[player2Choice] : '❓'
 
-    let description
+    let description = `⏳ Time has warped the battlefield...\n${player1} conjured ${player1Emoji}, \nwhile ${player2} conjured ${player2Emoji}.\n\n🌀 The cosmos have declared a timeout! 🌀`
+
     if (player1Choice && player2Choice) {
       const result = rpsResolver(player1Choice, player2Choice)
 
       const winnerAnnouncement =
         result === 'tie'
           ? "It's an epic stalemate! 🌟"
-          : result === 'player1'
-          ? `🏆 ${player1} triumphs! 🏆`
-          : `🎉 Victory for ${player2}! 🎉`
+          : `🎉 Victory for ${result === 'player1' ? player1 : player2}! 🎉`
+
       description = `🚀 The battle commences! 🚀\n\n${player1} summons ${player1Emoji} \nagainst ${player2}'s ${player2Emoji}!\n\n🌌 ${winnerAnnouncement}`
-    } else {
-      description = `⏳ Time has warped the battlefield...\n${player1} conjured ${player1Emoji}, \nwhile ${player2} conjured ${player2Emoji}.\n\n🌀 The cosmos have declared a timeout! 🌀`
     }
 
     await message.edit({
