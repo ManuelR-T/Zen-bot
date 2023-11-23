e384d6c1b1103146eb4253d72f2c1c0011bc5b2e
Manuel Tomé <manuel.tome@epitech.eu>
2023-11-23 16:54:06 +0100

    feat: try status

--- src/events/ready/index.ts
+++ src/events/ready/index.ts
@@ -1,16 +1,22 @@
-import { Events } from 'discord.js'
+import { ActivityType, Events } from 'discord.js'
 import { logger } from 'utils'
 
 import { Event, MyClient } from '@/types'
 
-import { client } from '../../'
-
-const setUserActivity = (): void => {
+const setUserActivity = (client: MyClient): void => {
   if (!client.user) {
     logger.error('Failed to get user.')
     process.exit(1)
   }
+  const arrayOfCommands = Array.from(client.commands.keys())
+
   client.user.setStatus('online')
+  client.user.setActivity({
+    name: `Try /${
+      arrayOfCommands[Math.floor(Math.random() * arrayOfCommands.length)]
+    }`,
+    type: ActivityType.Custom,
+  })
 }
 
 const listener = async (client: MyClient): Promise<void> => {
@@ -18,14 +24,14 @@ const listener = async (client: MyClient): Promise<void> => {
     logger.error('Failed to create client.')
     process.exit(1)
   }
-  if (!client.user) {
-    logger.error('Failed to get user.')
-    process.exit(1)
-  }
-  logger.info('Tune the discord bot')
 
-  setUserActivity()
-  logger.info('Bot is ready! (' + client?.user?.tag + ')')
+  logger.info('Tune the discord bot')
+  setUserActivity(client)
+  setTimeout(() => {
+    setUserActivity(client)
+  }, 600000)
+
+  logger.info(`Bot is ready! (${client.user?.tag})`)
 }
 
 export default {
